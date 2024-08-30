7a8b3afebe6310adc5ba84c16316bc2d08ccc548
Zulfiqar Mughul <zulfiqar.mughul@elev.ga.ntig.se>
2024-08-30 13:57:39 +0200

    första

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string name;
+string age;
+string country;
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Clear();
+
+Console.WriteLine("skriv in ditt namn: ");
+name = Console.ReadLine();
+;
+Console.WriteLine("Hur gammal är du?: ");
+age = Console.ReadLine();
+
+Console.WriteLine("Vart kommer du ifrån?");
+country = Console.ReadLine();
+
+Console.ForegroundColor = ConsoleColor.Red;
+// Console.WriteLine("Hello " + name + "!");
+ Console.WriteLine($"Hej {name} från {country}! Du är {age} år gammal");
+
+Console.ReadLine();
